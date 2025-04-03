139765b7629e681b1f0cc745ab3bd854f042c5a0
Geekwithahat <will.dubs.harries@gmail.com>
2025-04-04 00:11:13 +0100

    add group

--- bigManager/scripts/massRename.py
+++ bigManager/scripts/massRename.py
@@ -1,28 +1,33 @@
 import re
 
-cmds.select("Spices", hierarchy = True)
-selected = cmds.ls(selection=True)
 
-assetName = selected[0]
+def massRename(name) -> None :
+    cmds.select(name, hierarchy = True)
+    selected = cmds.ls(selection=True)
+    
+    assetName = selected[0]
+    
+    replace = {}
+    
+    for c in selected :
+         cN = re.findall("[A-Z, a-z 0-9, ^|_]*[a-z, A-Z]", c)[0]
+         if (cN == assetName) | ("|" in cN) | (re.search(".*Shape", cN) != None) | (c in replace):
+             continue
+         replace[c] = assetName + "_" + cN
+    
+    
+    for rn in selected : 
+        if (rn == assetName) | ('|' in rn) | ("Shape" in rn) | (assetName in rn):
+            continue
+        print(rn)
+        cmds.select(rn)
+        cmds.rename(replace[rn])
 
-replace = {}
 
-for c in selected :
-     cN = re.findall("[A-Z, a-z 0-9, ^|_]*[a-z, A-Z]", c)[0]
-     if (cN == assetName) | ("|" in cN) | (re.search(".*Shape", cN) != None) | (c in replace):
-         continue
-     replace[c] = assetName + "_" + cN
-# 
+def createFolder(folderName) -> None :
+    cmds.group(n=folderName)
+    massRename(folderName)
+   
 
-for rn in selected : 
-    if (rn == assetName) | ('|' in rn) | ("Shape" in rn) | (assetName in rn):
-        continue
-    print(rn)
-    cmds.select(rn)
-    cmds.rename(replace[rn])
-
-# get main name 
-
-# get new name, skip if contains an existing name
-
-# for each instance of old name replace with new name
+createFolder("A")
+    
