4afa78dda65647e0beacc3a68666b4048a9a0e3d
Geekwithahat <will.dubs.harries@gmail.com>
2025-04-03 16:05:13 +0100

    rename debug

--- bigManager/scripts/massRename.py
+++ bigManager/scripts/massRename.py
@@ -1,23 +1,28 @@
 import re
 
-cmds.select("comfy_chair", hierarchy = True)
+cmds.select("Spices", hierarchy = True)
 selected = cmds.ls(selection=True)
 
 assetName = selected[0]
 
-print(assetName != selected[0])
-
-print(selected)
+replace = {}
 
 for c in selected :
-    if c != assetName :
-        cN = re.
-        cmds.select(c)
-        newName = assetName + "_" + cN
-        newName = re.sub("[0-9]*$","",newName)
-        cmds.rename(assetName + "_" + cN)
+     cN = re.findall("[A-Z, a-z 0-9, ^|_]*[a-z, A-Z]", c)[0]
+     if (cN == assetName) | ("|" in cN) | (re.search(".*Shape", cN) != None) | (c in replace):
+         continue
+     replace[c] = assetName + "_" + cN
+# 
 
-# [A-Z, a-z 0-9]*[a-z, A-Z]
-    
-    
-    
+for rn in selected : 
+    if (rn == assetName) | ('|' in rn) | ("Shape" in rn) | (assetName in rn):
+        continue
+    print(rn)
+    cmds.select(rn)
+    cmds.rename(replace[rn])
+
+# get main name 
+
+# get new name, skip if contains an existing name
+
+# for each instance of old name replace with new name
