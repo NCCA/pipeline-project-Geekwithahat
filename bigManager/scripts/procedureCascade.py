bbd290216a33deac46f33da84e2578c0dec5d26a
Geekwithahat <will.dubs.harries@gmail.com>
2025-05-14 23:17:13 +0100

    add procedure debugging

--- bigManager/scripts/procedureCascade.py
+++ bigManager/scripts/procedureCascade.py
@@ -34,34 +34,43 @@ import maya.mel as mel
 # ------ FUNCTION CASCADE ------
 
 def cascadeFunctions(Folder, Code) :
-    # every object
-    cmds.select(clear=True)
-    Reg = "^"+Folder+"_.*"
-    for i in cmds.ls(typ="transform") :
-        if(re.findall(Reg, i) != []):
-            cmds.select(i, add=True)
-    x = cmds.ls(sl=True, typ="transform")
+    try:
+        # every object
+        cmds.select(clear=True)
+        Reg = "^"+Folder+"_.*"
+        for i in cmds.ls(typ="transform") :
+            if(re.findall(Reg, i) != []):
+                cmds.select(i, add=True)
+        x = cmds.ls(sl=True, typ="transform")
 
-    # apply function to each
-    for object in x :
-        cmds.select(object)
-        exec(Code)
+        # apply function to each
+        for object in x :
+            cmds.select(object)
+            exec(Code)
+    except:
+        cmds.warning("Provided code error.")
 
 
 
 # ------ REPEAT PREVIOUS COMMAND ------
 
 def repeatLast(Folder) :
-    # every object
-    cmds.select(clear=True)
-    Reg = "^"+Folder+"_.*"
-    for i in cmds.ls(typ="transform") :
-        if(re.findall(Reg, i) != []):
-            cmds.select(i, add=True)
-    x = cmds.ls(sl=True, typ="transform")
+    try:
+        if(cmds.getAttr(Folder + ".FolderFlag")) : 
+            # every object
+            cmds.select(clear=True)
+            Reg = "^"+Folder+"_.*"
+            for i in cmds.ls(typ="transform") :
+                if(re.findall(Reg, i) != []):
+                    cmds.select(i, add=True)
+            x = cmds.ls(sl=True, typ="transform")
 
-    # apply previous function for each object in folder
-    for o in x :
-        cmds.select(o)
-        mel.eval("RepeatLast")        
+            # apply previous function for each object in folder
+            for o in x :
+                cmds.select(o)
+                mel.eval("RepeatLast")
+        else:
+            cmds.warning("Please provide an appropriate folder.") 
+    except:
+        cmds.warning("Please provide an appropriate folder.")      
     
